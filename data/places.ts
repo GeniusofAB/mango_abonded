112a1065c55f5203778b1017066085885145c45f
GeniusofAB <156613845+GeniusofAB@users.noreply.github.com>
2025-09-18 19:25:00 +0400

    Update places.ts

--- data/places.ts
+++ data/places.ts
@@ -3,8 +3,8 @@ import { AbandonedPlace } from '../types';
 export const ABANDONED_PLACES: AbandonedPlace[] = [
   {
     id: 'example-1',
-    title: 'Заброшенная текстильная фабрика',
-    description: 'Старая текстильная фабрика времен СССР, закрытая в начале 90-х годов. Большое промышленное здание с частично сохранившимся оборудованием. Интересные граффити на стенах. Здание находится в относительно хорошем состоянии, но требует осторожности при исследовании из-за старых конструкций.',
+    title: 'pizda',
+    description: 'net',
     images: [
       'https://images.unsplash.com/photo-1667328931918-b6362ed95c1e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhYmFuZG9uZWQlMjBmYWN0b3J5JTIwYmF0dW1pJTIwZ2VvcmdpYSUyMGluZHVzdHJpYWx8ZW58MXx8fHwxNzU3Nzk4OTY1fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral'
     ],
